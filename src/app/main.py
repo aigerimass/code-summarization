c7741440357d857dc7ad7ec1a58937bbd6350f26
aigerimass <assylkhanova0@gmail.com>
2023-12-02 02:26:19 +0000

    added history endpoint. configured static for fastapi. generalized style with css file.

--- src/app/main.py
+++ src/app/main.py
@@ -2,6 +2,8 @@ import uvicorn
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 
 from app.model import predict
 
@@ -11,8 +13,12 @@ app = FastAPI(
     title="CodeSummarization"
 )
 
+app.mount("/app/static", StaticFiles(directory=Path(__file__).parent.parent.absolute() / "app/static"), name="static")
 # Jinja2 templates configuration
 templates = Jinja2Templates(directory="src/app/templates")
+
+# Request history
+request_history = []
 
 
 # initialize model
@@ -26,8 +32,16 @@ async def read_form(request: Request):
 async def process_form(request: Request, code: str = Form(...)):
     result_summary = predict(code)
 
+    # Add request to history
+    request_history.append({"code": code, "result_summary": result_summary})
+
     return templates.TemplateResponse("result.html",
                                       {"request": request, "code": code, "result_summary": result_summary})
+
+
+@app.get("/history", response_class=HTMLResponse)
+async def get_history(request: Request):
+    return templates.TemplateResponse("history.html", {"request": request, "request_history": request_history})
 
 
 def start():
