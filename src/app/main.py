871b2ea765607767d5da3ef48510f7b243588522
aigerimass <assylkhanova0@gmail.com>
2023-12-02 22:02:58 +0000

    added database for requests and code summarizations

--- src/app/main.py
+++ src/app/main.py
@@ -4,7 +4,8 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
-
+from app.dbmodels import RequestHistory
+from app.database import SessionLocal
 from app.model import predict
 
 # import model
@@ -17,11 +18,6 @@ app.mount("/app/static", StaticFiles(directory=Path(__file__).parent.parent.abso
 # Jinja2 templates configuration
 templates = Jinja2Templates(directory="src/app/templates")
 
-# Request history
-request_history = []
-
-
-# initialize model
 
 @app.get("/", response_class=HTMLResponse)
 async def read_form(request: Request):
@@ -33,7 +29,12 @@ async def process_form(request: Request, code: str = Form(...)):
     result_summary = predict(code)
 
     # Add request to history
-    request_history.append({"code": code, "result_summary": result_summary})
+    db = SessionLocal()
+    db_request = RequestHistory(code=code, result_summary=result_summary)
+    db.add(db_request)
+    db.commit()
+    db.refresh(db_request)
+    db.close()
 
     return templates.TemplateResponse("result.html",
                                       {"request": request, "code": code, "result_summary": result_summary})
@@ -41,6 +42,10 @@ async def process_form(request: Request, code: str = Form(...)):
 
 @app.get("/history", response_class=HTMLResponse)
 async def get_history(request: Request):
+    db = SessionLocal()
+    request_history = db.query(RequestHistory).all()
+    db.close()
+
     return templates.TemplateResponse("history.html", {"request": request, "request_history": request_history})
 
 
